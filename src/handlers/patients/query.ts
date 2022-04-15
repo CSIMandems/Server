d9695035acf7e393e0cb5196608bafe53d69f0d9
Daniel Shwan <dannyshwan@gmail.com>
2022-04-15 18:15:43 -0400

    hot fix 2

--- src/handlers/patients/query.ts
+++ src/handlers/patients/query.ts
@@ -6,14 +6,12 @@ export const selectAllPatients = () => {
 };
 
 export const createPatient = (p: Patient) => {
-	return `INSERT INTO patient (first_name, middle_name, last_name, date_of_birth, gender,
-    social_security_number, insurance_number,
-      civic_number, street_name, city, province, postal_code,
-     phone_number, email_address, user_id)
+	return `INSERT INTO patient (first_name, middle_name, 
+    last_name, date_of_birth, gender, social_security_number, insurance_number, civic_number, street_name, city, province, postal_code, phone_number, email_address, user_id, responsible_party_id)
 VALUES ('${p.first_name}', '${p.middle_name}', '${p.last_name}', '${p.date_of_birth}', '${p.gender}',
     '${p.ssn}', '${p.insurance_number}',
       '${p.civic_number}', '${p.street_name}', '${p.city}', '${p.province}', '${p.postal_code}',
-     '${p.phone_number}', '${p.email_address}', '${p.user_id}')
+     '${p.phone_number}', '${p.email_address}', '${p.user_id}', ${p.responsible_party_id})
 RETURNING patient_id`;
 };
 
